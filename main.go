3bbc9df2cf9b523a330e95be13d9eea67c7dad19
浜田　俊祐 <hamashun0820@icloud.com>
2024-11-10 17:01:56 +0900

    検知

--- main.go
+++ main.go
@@ -7,11 +7,15 @@ import (
 	_ "embed"
 	"fmt"
 	"image"
+	"image/color"
 	"log"
 	"syscall/js"
 
+	pigo "github.com/esimov/pigo/core"
+	"github.com/esimov/pigo/wasm/detector"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
 var (
@@ -19,6 +23,7 @@ var (
 	stream js.Value
 	canvas js.Value
 	ctx    js.Value
+	det    *detector.Detector
 )
 
 const (
@@ -27,6 +32,11 @@ const (
 )
 
 func init() {
+	det = detector.NewDetector()
+	if err := det.UnpackCascades(); err != nil {
+		log.Fatal(err)
+	}
+
 	doc := js.Global().Get("document")
 	video = doc.Call("createElement", "video")
 	canvas = doc.Call("createElement", "canvas")
@@ -63,6 +73,9 @@ type Game struct {
 	err         error
 	drawImg     *ebiten.Image
 	cx, cy, rad float64
+	leftEye     *pigo.Puploc
+	rightEye    *pigo.Puploc
+	faceNum     int
 }
 
 func loadImage(data []byte) (*ebiten.Image, error) {
@@ -87,6 +100,17 @@ func (g *Game) Update() error {
 		return nil
 	}
 	goBin := fetchVideoFrame()
+	pixels := rgbaToGrayscale(goBin, ScreenWidth, ScreenHeight)
+	// widht, height が逆
+	dets := det.DetectFaces(pixels, ScreenHeight, ScreenWidth)
+	g.faceNum = len(dets)
+	for i := 0; i < g.faceNum; i++ {
+		g.cx, g.cy, g.rad = float64(dets[i][1]), float64(dets[i][0]), float64(dets[i][2])*1.5
+	}
+	if g.faceNum > 0 {
+		g.leftEye = det.DetectLeftPupil(dets[0])
+		g.rightEye = det.DetectRightPupil(dets[0])
+	}
 	g.drawImg = ebiten.NewImageFromImage(newImage(goBin, ScreenWidth, ScreenHeight))
 	return nil
 }
@@ -95,8 +119,19 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	if g.err != nil {
 		return
 	}
+
 	screen.DrawImage(g.drawImg, nil)
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %f", ebiten.CurrentFPS()))
+	// 目の位置に丸を描画
+	if g.faceNum > 0 {
+		c := color.RGBA{0xff, 0, 0, 0xff}
+		vector.DrawFilledCircle(screen, float32(g.cx), float32(g.cy), 5, c, true)
+		vector.DrawFilledCircle(screen, float32(g.leftEye.Col), float32(g.leftEye.Row), 5, c, true)
+		vector.DrawFilledCircle(screen, float32(g.rightEye.Col), float32(g.rightEye.Row), 5, c, true)
+	}
+
+	if g.faceNum > 0 {
+		ebitenutil.DebugPrint(screen, fmt.Sprintf("faceNum: %d\nFPS: %f\nfx: %f, fy: %f", g.faceNum, ebiten.ActualFPS(), g.cx, g.cy))
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
